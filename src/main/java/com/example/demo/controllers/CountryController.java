dac2b498a1d1e05853a1382336bc0c170fdbabe1
Mexolius <39342674+Mexolius@users.noreply.github.com>
2020-06-03 23:10:55 +0200

    Changing Mapping policy

--- src/main/java/com/example/demo/controllers/CountryController.java
+++ src/main/java/com/example/demo/controllers/CountryController.java
@@ -5,9 +5,7 @@ import com.example.demo.models.DataFromDay;
 import com.example.demo.models.EpidemyDay;
 import com.example.demo.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.List;
@@ -18,45 +16,54 @@ public class CountryController {
     @Autowired
     private CountryService countryService;
 
-    @RequestMapping("/countries")
+    @CrossOrigin
+    @GetMapping("/countries")
     public List<Country> getAllCountries(){
         return countryService.findAllCountries();
     }
 
-    @RequestMapping("/countries/{name}")
+    @CrossOrigin
+    @GetMapping("/countries/{name}")
     public Country getCountry(@PathVariable String name){
         return countryService.findByCountryname(name).orElse(null);
     }
 
-    @RequestMapping("/countries/{name}/days/{date}")
+    @CrossOrigin
+    @GetMapping("/countries/{name}/days/{date}")
     public EpidemyDay getEpidemyDay(@PathVariable String name, @PathVariable String date) throws ParseException {
         return countryService.findByCountryAndDate(name,date).orElse(null);
     }
 
-    @RequestMapping("/countries/{name}/days")
+    @CrossOrigin
+    @GetMapping("/countries/{name}/days")
     public List<EpidemyDay> getCountryEpidemyDays(@PathVariable String name){
         return countryService.findAllDaysByCountry(name);
     }
 
-    @RequestMapping("/days")
+    @CrossOrigin
+    @GetMapping("/days")
     public List<EpidemyDay> getAllDays(){
         return countryService.findAllDays();
     }
 
-    @RequestMapping("/summary/current")
+    @CrossOrigin
+    @GetMapping("/summary/current")
     public DataFromDay getSummary(){
         return countryService.getEpidemySummary();
     }
 
-    @RequestMapping("/summary/total")
+    @CrossOrigin
+    @GetMapping("/summary/total")
     public List<DataFromDay> getEveryDaySummary() {return countryService.getEveryDaySummary();}
 
-    @RequestMapping("/countries/{name}/days/difference/{date}")
+    @CrossOrigin
+    @GetMapping("/countries/{name}/days/difference/{date}")
     public EpidemyDay getCountryDayDifference(@PathVariable String name, @PathVariable String date) throws ParseException {
         return countryService.getCountryDayDifference(name,date).orElse(null);
     }
 
-    @RequestMapping("/countries/{name}/days/difference")
+    @CrossOrigin
+    @GetMapping("/countries/{name}/days/difference")
     public List<EpidemyDay> getCountryDaysDifference(@PathVariable String name){
         return countryService.getAllDaysDifference(name);
     }
